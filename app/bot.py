3a87bdd5474698789609051312406df64b3bf5c6
Abhijit Roy <dalvath3700@gmail.com>
2019-08-15 20:58:12 +0530

    requirements.txt, procfile, data, app.py modified/added

---
+++ app/bot.py
@@ -0,0 +1,30 @@
+import botogram
+from input import *
+
+bot = botogram.create(API_key)
+bot.about = "This is a Key Provider Bot. \nThis provides keys (max. 6 per day) to a user based on information - date, time, user, country"
+bot.owner = "@abhi3700"
+
+@bot.command("hello")
+def hello_command(chat, message, args):
+    """Says Hi to your message"""
+    chat.send("Hey! what's up?")
+
+@bot.command("requestkey")
+def requestkey_command(chat, message, args):
+    """Provides key based on user information"""
+    chat.send("Okay! But I need some of your information. \nUse /sendinfo command.")
+
+"""
+TODO: Button popping from below
+    - Username: when clicked, `username` saved in the database & show msg: "Username noted."
+    - Datetime: when clicked, `Datetime` saved in the database & show msg: "Datetime noted."
+    - Location: When clicked, `location` saved in the database & show msg: "Location noted."
+"""
+@bot.command("sendinfo")
+def sendinfo_command(chat, message, args):
+    """User has to click a button for giving information - Username, Datetime, Location"""
+    chat.send("Please, select one of the buttons popping below.")
+
+if __name__ == "__main__":
+    bot.run()
