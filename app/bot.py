0fbb3a51e755efa277e3d16bb98fa4df7e19dad5
Abhijit Roy <dalvath3700@gmail.com>
2019-08-17 22:24:49 +0530

    updated app/bot.py

--- app/bot.py
+++ app/bot.py
@@ -5,10 +5,27 @@ bot = botogram.create(API_key)
 bot.about = "This is a Key Provider Bot. \nThis provides keys (max. 6 per day) to a user based on information - date, time, user, country"
 bot.owner = "@abhi3700"
 
+# =========================================================================================================================================
 @bot.command("requestkey")
 def requestkey_command(chat, message, args):
+    """
+    TODO: Check if the group is/not in a group
+    If(message.sender.group != group_name):
+        chat.send("You need to be in the group,\n" + group_link + " in order to get key")
+    else:
+        chat.send("Okay! But I need some of your information. \nUse /sendinfo command.")
+    """
     """Provides key based on user information"""
     chat.send("Okay! But I need some of your information. \nUse /sendinfo command.")
+
+"""
+parse the function o/p as argument into `btns.callback` line
+"data":  string
+"return": string
+"""
+def save_username(data):
+    db_status = ""
+    return db_status
 
 """
 TODO: Button popping from below
@@ -20,14 +37,16 @@ TODO: Button popping from below
 def sendinfo_command(chat, message):
     """User has to click a button for giving information - Username, Datetime, Location"""
     btns = botogram.Buttons()
+    # TODO: replace `message.sender.username` with `save_username(message.sender.username)`
     btns[0].callback("Username", "username", message.sender.username)     # button - Username
     btns[1].callback("Location", "location")     # button - Location
-    chat.send("Please, select one of the buttons popping below." + message.sender.username, attach= btns)
+    chat.send("Please, select one of the buttons popping below.", attach= btns)
 
 @bot.callback("username")
 def username_callback(query, data):
     # query.notify("<username> saved.")
-    query.notify("<username> saved: " + data)
+    query.notify("<username>: " + data + " saved.")
+    query.notify("<username>: " + type(data) + " saved.")
 
 @bot.callback("location")
 def location_callback(query, chat, message):
@@ -35,7 +54,6 @@ def location_callback(query, chat, message):
     # chat.send(str(loc.longitude) + "\n" + str(loc.latitude))    # test
     query.notify("<location> saved.")
 
+# ================================================MAIN===========================================================================
 if __name__ == "__main__":
     bot.run()
-    # user = botogram.User()
-    # print(user.username)
