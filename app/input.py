e4d85fde96d9e6c070850fa90a7dfa7957f0fd98
Abhijit Roy <dalvath3700@gmail.com>
2019-08-18 23:18:07 +0530

    REDIS_URL added for redis db

--- app/input.py
+++ app/input.py
@@ -1,1 +1,5 @@
+# Take it from creating Bot API token in BotFather's chat
 API_key = "979871020:AAGWLY7hTPuKH1EiNqD592AzHS01nhU-RBI"
+
+# Capture using `$ heroku config | grep REDIS` from the terminal at App's root dir
+REDIS_URL = "redis://h:p478e373c898b3eb564e2a14303db09c415cfcacffa638404d3378e24497c2ffb@ec2-3-222-131-127.compute-1.amazonaws.com:13819"
