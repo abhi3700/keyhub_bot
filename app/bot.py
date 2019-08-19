6e39873932ae357e7fb50260a12bee04e2a5a561
Abhijit Roy <dalvath3700@gmail.com>
2019-08-19 19:48:51 +0530

    app/bot.py added

--- app/bot.py
+++ app/bot.py
@@ -1,9 +1,15 @@
 import botogram
+import redis
+import os
 from input import *
+
 
 bot = botogram.create(API_key)
 bot.about = "This is a Key Provider Bot. \nThis provides keys (max. 6 per day) to a user based on information - date, time, user, country"
 bot.owner = "@abhi3700"
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# define Redis database
+r = redis.from_url(REDIS_URL)
 
 # =========================================================================================================================================
 @bot.command("requestkey")
@@ -35,19 +41,25 @@ def sendinfo_command(chat, message, args):
     
     chat.send("Please, select one of the buttons popping below.", attach= btns)
 
+# define empty dictionary for JSON object in Redis db
+d_username = {}
+
 @bot.callback("username")
 def username_callback(query, chat, message):
     user = query.sender
-    chat.send(user.username)    # test 
-    query.notify("{username} saved.".format(username=user.username))
+    username = user.username
+    r.hmset(username, dict(username= username))
+    chat.send(r.hget(username, "username").decode('utf-8'))    # test 
+    query.notify("{username} saved.".format(username=r.hget(username, "username").decode('utf-8')))
 
 @bot.callback("location")
-def location_callback(query, chat, message, location):
-    loc = message.location
-    lat = loc.latitude
-    lng = loc.longitude
-    chat.send(str(lat) + " & " + str(lng))      # test
-    query.notify("{latitude} & {longitude} saved.".format(latitude=str(lat), longitude=str(lng)))
+def location_callback(query, chat, message):
+    # loc = message.location
+    # lat = loc.latitude
+    # lng = loc.longitude
+    # chat.send(str(lat) + " & " + str(lng))      # test
+    # query.notify("{latitude} & {longitude} saved.".format(latitude=str(lat), longitude=str(lng)))
+    query.notify("<location> saved.")
 
 # ================================================MAIN===========================================================================
 if __name__ == "__main__":
