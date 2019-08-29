4cf80b5fb2d43f10393009ed5dc5c8fc14c98abd
Abhijit Roy <dalvath3700@gmail.com>
2019-08-29 10:03:46 +0530

    added keyboard for sharing phone no., db_excel.py added

--- app/bot.py
+++ app/bot.py
@@ -4,14 +4,16 @@ import redis
 import requests
 from input import *
 
-
+# -------------------------------------------------------About Bot--------------------------------------------------------------------
 bot = botogram.create(API_key)
 bot.about = "This is a Key Provider Bot. \nThis provides keys (max. 2 per day) to a user based on information - username, location"
 bot.owner = "@abhi3700"
-# ----------------------------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------Redis DB------------------------------------------------------------------------
 # define Redis database
 r = redis.from_url(REDIS_URL)
 
+# -------------------------------------------------------'phone' global var------------------------------------------------------------------------
+phone_global = ""
 # =========================================================================================================================================
 @bot.command("requestkey")
 def requestkey_command(chat, message, args):
@@ -34,12 +36,69 @@ def requestkey_command(chat, message, args):
 # =======================================================Products===========================================================================
 @bot.callback("producta")
 def producta_callback(query, chat, message):
+    chat.send("First of all, please share your phone no. via /sharephone command.")
     chat.send("Okay! But I need some of your information. \nUse /sendinfo command.")
 
 @bot.callback("productb")
 def productb_callback(query, chat, message):
+    chat.send("First of all, please share your phone no. via /sharephone command.")
     chat.send("Okay! But I need some of your information. \nUse /sendinfo command.")
 
+# =======================================================Share phone via keyboard===========================================================================
+@bot.command("sharephone")
+def sharephone_command(chat, message, args):
+    bot.api.call('sendMessage', {
+        'chat_id': chat.id,
+        'text': 'Please click on keyboard below to share your phone no.',
+        'reply_markup': json.dumps({
+            'keyboard': [
+                [
+                    {'text': 'Phone'},
+                ],
+                [
+                    {
+                        'text': 'Share your phone number',
+                        'request_contact': True,
+                    },
+                ],
+            ],
+            # These 3 parameters below are optional
+            # See https://core.telegram.org/bots/api#replykeyboardmarkup
+            'resize_keyboard': True,
+            'one_time_keyboard': True,
+            'selective': True,
+        }),
+    })
+
+@bot.message_matches('Phone')
+def action_a_clicked(chat):
+    chat.send('Processing Action A... This won\'t trigger @bot.process_message')
+
+
+@bot.process_message
+def button_messages_are_like_normal_messages(chat, message):
+    # if message.text:
+    #     chat.send('You choose %s' % message.text)
+    # elif message.location:
+    #     chat.send('You choose to send your location: %s %s' % (message.location.latitude, message.location.longitude))
+    if message.contact:
+        phone_global = message.contact.phone_number
+        chat.send('You choose to send your contact: %s' % message.contact.phone_number)
+    chat.send('Press /removekeyboard to remove the annoying keyboard')
+
+@bot.command("removekeyboard")
+def removekeyboard_command(chat, message):
+    bot.api.call('sendMessage', {
+        'chat_id': chat.id,
+        'reply_to_message': message.id,
+        'text': 'This message removes the keyboard',
+        'reply_markup': json.dumps({
+            'remove_keyboard': True,
+            # This 1 parameter below is optional
+            # See https://core.telegram.org/bots/api#replykeyboardremove
+            'selective': True,
+        })
+    })
 # ========================================================User Information==================================================================
 """
 TODO: Button popping from below
@@ -62,16 +121,16 @@ def sendinfo_command(chat, message, args):
 @bot.callback("username")
 def username_callback(query, chat, message):
     user = query.sender
-    username = user.username
-    phoneno = username.contact.phone_no
+    uname = user.username
+    # phoneno = username.contact.phone_no
 
     # r.hmset(username, dict(username= username))
     msg_username = ""   # initialize
     # msg_username = r.hget(username, "username").decode('utf-8')
-    msg_username = username 
+    # msg_username = username 
 
-    query.notify("username: {username} saved.".format(username=msg_username))
-    chat.send("Phone no.: {phone} saved.".format(phone= phoneno))
+    query.notify("username: {username} saved.".format(username= uname))
+    chat.send("Phone no.: {phone} saved.".format(phone= chat.contact.phone_number))
     # chat.send("{username} saved.".format(username=msg_username))
     # chat.send("Now, please share your location by clicking the BUTTON above")
 
@@ -87,6 +146,7 @@ def location_callback(query, chat, message):
 
         # Fetch 'product_key' from DB (in excel) with username & country & phone as empty.
         chat.send("your key is <product_key>")
+        # TODO: After this, corresponding to this product_key save infos. - username, location, phoneno is filled in Excel DB.
     else:
         chat.send("Connection ERROR! Please try again later. \nAlso, you can raise query at @abhi3700")
     
--- app/db_excel.py
+++ app/db_excel.py
@@ -1,7 +1,28 @@
+"""
+TODO:
+Test the followings:
+- [ ] fetch the product key where all 'country', 'username', 'phoneno' are NaN
+- [ ] write the 'country', 'username', 'phoneno' values to CSV file
+- [ ] fetch the product keys where df["username"] == 'abhi3700'
+"""
 import pandas as pd
+import numpy as np
 
 df_a = pd.read_csv("../keys/A.csv")
 print(df_a.head())
+# print(df_a['country'].tolist())
+# print(df_a['username'].tolist())
+# print(df_a['phoneno'].tolist())
 
 df_b = pd.read_csv("../keys/B.csv")
 print(df_b.head())
+
+print(df_a.loc[~(df_a['country'] == 'india')]['phoneno'])
+
+# print(np.nan)
+
+# print(df_a[~(df_a['country'] is np.nan) & ~(df_a['username'] is np.nan) & ~(df_a['phoneno'] is np.nan)])
+print(df_a.loc[(df_a['country'] == np.nan)]['phoneno'])
+
+# print(df_a_new.head())
+
