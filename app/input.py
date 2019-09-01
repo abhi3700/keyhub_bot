42a16ca451ea068acf6d9ae6f5b08fd554ea627a
Abhijit Roy <dalvath3700@gmail.com>
2019-09-01 11:46:37 +0530

    redis URL updated due to new repo creation

--- app/input.py
+++ app/input.py
@@ -2,7 +2,7 @@
 API_key = "979871020:AAGWLY7hTPuKH1EiNqD592AzHS01nhU-RBI"
 
 # Capture using `$ heroku config | grep REDIS` from the terminal at App's root dir
-REDIS_URL = "redis://h:p478e373c898b3eb564e2a14303db09c415cfcacffa638404d3378e24497c2ffb@ec2-3-222-131-127.compute-1.amazonaws.com:13819"
+REDIS_URL = "redis://h:pd4a2fa90f5a63058400930ffd21f5864312b2a174061846e13543a79eb1fdd81@ec2-54-77-8-133.eu-west-1.compute.amazonaws.com:18179"
 
 # Accesssing 'country_name' from this ipdata URL
 geo_URL = 'https://api.ipdata.co/?api-key=858097a26b070972fd6ecabb3f36421ff55eaf8337143a4b15ecaf39'
