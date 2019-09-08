3dd2f252fadabb5ab36243e18b0def9833d66d61
Abhijit Roy <dalvath3700@gmail.com>
2019-09-08 13:14:45 +0530

    cfg_uri_psql link added

--- app/input.py
+++ app/input.py
@@ -4,5 +4,9 @@ API_key = "979871020:AAGWLY7hTPuKH1EiNqD592AzHS01nhU-RBI"
 # Capture using `$ heroku config | grep REDIS` from the terminal at App's root dir
 REDIS_URL = "redis://h:pd4a2fa90f5a63058400930ffd21f5864312b2a174061846e13543a79eb1fdd81@ec2-54-77-8-133.eu-west-1.compute.amazonaws.com:18179"
 
+# Capture using `$ heroku config` from the terminal at App's root dir
+DATABASE_URL = "postgres://sncwwevyyzviez:7569a516be40f3f5d62f6d6a8818556771c5f1ade86096b96dabeb01bef14c37@ec2-54-247-96-169.eu-west-1.compute.amazonaws.com:5432/dm8m5ustplad3"
+cfg_uri_psql = "postgresql+psycopg2://sncwwevyyzviez:7569a516be40f3f5d62f6d6a8818556771c5f1ade86096b96dabeb01bef14c37@ec2-54-247-96-169.eu-west-1.compute.amazonaws.com:5432/dm8m5ustplad3"
+
 # Accesssing 'country_name' from this ipdata URL
 geo_URL = 'https://api.ipdata.co/?api-key=858097a26b070972fd6ecabb3f36421ff55eaf8337143a4b15ecaf39'
