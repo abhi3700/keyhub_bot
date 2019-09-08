079b66bd37807c613cea80bd2245047d22fb8d0c
Abhijit Roy <dalvath3700@gmail.com>
2019-09-08 13:30:23 +0530

    comments added

--- app/db_pgres_up.py
+++ app/db_pgres_up.py
@@ -6,14 +6,14 @@ import pandas as pd
 import d6tstack.utils as du
 from input import *
 
-
+# ==============================Read CSV into dataframe=============
 df_a = pd.read_csv('../keys/A.csv')
 df_b = pd.read_csv('../keys/B.csv')
 df_c = pd.read_csv('../keys/C.csv')
 df_d = pd.read_csv('../keys/D.csv')
 df_e = pd.read_csv('../keys/E.csv')
 
-
+# ==============================Upload dataframe to PostgreSQL database=============
 du.pd_to_psql(df_a, cfg_uri_psql, 'product_a', if_exists='replace')
 du.pd_to_psql(df_b, cfg_uri_psql, 'product_b', if_exists='replace')
 du.pd_to_psql(df_c, cfg_uri_psql, 'product_c', if_exists='replace')
