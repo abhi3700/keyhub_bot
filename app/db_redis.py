0c6d372f71975307902928cc8aef36ffc5d2a990
Abhijit Roy <dalvath3700@gmail.com>
2019-09-11 13:11:31 +0530

    added "find out root key name, based on `username`"

--- app/db_redis.py
+++ app/db_redis.py
@@ -35,7 +35,7 @@ r = redis.from_url(REDIS_URL)
 # print(json.loads(r.hget(phoneno, "ProductA").decode('utf-8')).get("username"))
 # print(json.dumps(r.get(phoneno), indent= 2))
 # r.delete('DUMMY')
-print(r.keys())
+# print(r.keys())
 
 # for key in r.keys():
 #     # print(key.decode('utf-8'))
@@ -46,11 +46,60 @@ print(r.keys())
 #     # print(json.loads(r.hget(phone_global, "ProductA").decode('utf-8')).get("country"))
 
 # print(r.keys())
-phone_global = '749832789'
-phone_global1 = '542354'
-key = 'Alltest28345237845238714'
-r.set(phone_global, "")
-r.hset(phone_global1, "ProductA", json.dumps(dict(key= key)))
 
+# phoneno1 = '+918146734455'
+# uname1 = 'abhi3701'
+
+# phoneno2 = '+918147424326734455'
+# uname2 = 'abhi3702'
+
+# phoneno3 = '+91565461474243267344'
+# uname3 = 'abhi3703'
+
+# r.hset(phoneno1, "product_a", json.dumps(dict(username= uname1)))
+# r.hset(phoneno2, "product_a", json.dumps(dict(username= uname2)))
+# r.hset(phoneno3, "product_a", json.dumps(dict(username= uname3)))
+
+# print(json.loads(r.hget(phoneno1, "product_a").decode('utf-8')).get('username'))
+# OR
+# print(json.loads(r.hget(phoneno1, "product_a").decode('utf-8'))['username'])
+# print(type(json.loads(r.hget(phoneno, "product_a").decode('utf-8'))))     # dict
+
+# dict_nested2_val2 = json.loads(r.hget(phoneno, "product_a").decode('utf-8'))
+# key_nested2 = ""
+
+# try:
+#     key_nested2 = list(dict_nested2_val2.keys())[list(dict_nested2_val2.values()).index('abhi3701')]       # get the key corresponding to value - 'India'
+# except ValueError:      # ignore this exception error when item not found
+#     pass
+# print(key_nested2)
+
+
+# phoneno2 = '+913257864235'
+# r.hset(phoneno2, "username", uname)
+# key_phone = ""
+# for k in r.keys():
+#     # print(k.decode('utf-8'))
+#     dict_nested2_val2 = json.loads(r.hget(k.decode('utf-8'), "product_a"))
+#     if dict_nested2_val2['username'] == 'abhi3700':
+#         key_phone = k.decode('utf-8')
+# print(key_phone)
+# if key_phone == "":
+#     print("Username doesn't exist")
+    # try:
+    #     key_nested2 = list(dict_nested2_val2.keys())[list(dict_nested2_val2.values()).index('abhi3701')]       # get the key corresponding to value - 'India'
+    # except ValueError:      # ignore this exception error when item not found
+    #     pass
 
 print(r.keys())
+# delete all stored keys
+# for k in r.keys():
+#     r.delete(k)
+
+for k in r.keys():
+    k_decoded = k.decode('utf-8')
+    print(json.loads(r.hget(k_decoded, "product_a").decode('utf-8')))
+
+print(json.loads(r.hget('+918146210685', "product_a").decode('utf-8')))
+
+print(type(str(datetime.date.today())))
